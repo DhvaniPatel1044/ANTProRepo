022580a8e73ebb5276748fba79b9fca2f589ad09
Dhvani Patel <pateldhvani440@gmail.com>
2025-07-18 11:24:37 +0530

    Uploading the local automation code to GitHubRepo

---
+++ src/packOne/ClassFour.java
@@ -0,0 +1,24 @@
+
+package packOne;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassFour {
+	
+	@Test
+	public void methodFourA() {
+		
+		System.out.println("Inside methodFourA of ClassFour");
+		 System.setProperty("webdriver.chrome.driver","Driver//chromedriver.exe");
+		 
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("http://www.yahoo.com");
+		 
+		 driver.close();
+		
+	}
+
+}
